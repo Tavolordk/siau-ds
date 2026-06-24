93968b97a81a835291cd95fe5706f5cfe8e72171
tavolordk <oleaoctavio23@gmail.com>
2026-06-24 13:30:47 -0600

    configuracion proxy url absoluta

--- src/app/core/http/auth-api-base-url.token.ts
+++ src/app/core/http/auth-api-base-url.token.ts
@@ -2,5 +2,5 @@ import { InjectionToken } from '@angular/core';
 
 export const AUTH_API_BASE_URL = new InjectionToken<string>('AUTH_API_BASE_URL', {
     providedIn: 'root',
-    factory: () => 'http://10.241.34.10:3240',
+    factory: () => '/auth-api',
 });
--- src/app/core/http/captcha-api-base-url.token.ts
+++ src/app/core/http/captcha-api-base-url.token.ts
@@ -2,5 +2,5 @@ import { InjectionToken } from '@angular/core';
 
 export const CAPTCHA_API_BASE_URL = new InjectionToken<string>('CAPTCHA_API_BASE_URL', {
     providedIn: 'root',
-    factory: () => 'http://10.237.3.44:3206',
+    factory: () => '/captcha-api',
 });
